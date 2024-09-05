8e74d0fbf74a82dbf2680a616c0a471be0d24bf0
aalvarezd <jantonio.alvarezd95@gmail.com>
2024-09-04 19:01:38 -0500

    AAD: fix 040924

---
+++ Vetsus.MVC/Controllers/ManagementController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vetsus.MVC.Controllers
+{
+    public class ManagementController : Controller
+    {
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+        public IActionResult Owners()
+        {
+            return View();
+        }
+    }
+}
