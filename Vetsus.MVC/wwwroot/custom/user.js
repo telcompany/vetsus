d32f6d5a21a9cf6d029cf096c308b0376a9b83ec
aalvarezd <jantonio.alvarezd95@gmail.com>
2024-09-07 19:20:56 -0500

    AAD: fix 070924

--- Vetsus.MVC/wwwroot/custom/user.js
+++ Vetsus.MVC/wwwroot/custom/user.js
@@ -14,3 +14,35 @@ function ajaxRequest(params) {
         params.success(res)
     })
 }
+
+function actionFormatter(id, row, index) {
+    var userId = "'" + id + "'";
+    return [
+        '<a href="javascript:void(0)" title="Editar" onclick="editUser(' + userId + ')"',
+        '<i class="fa fa-pencil fa-2x"></i>',
+        '</a>  ',
+        '&nbsp;&nbsp;',
+        '<a href="javascript:void(0)" title="Eliminar" onclick="deleteUser(' + userId + ')">',
+        '<i class="fa fa-trash fa-2x"></i>',
+        '</a>'
+    ].join('')
+}
+
+function addUser() {
+    $('#lblTitleModal').text('Nuevo usuario');
+    $('#addUserModal').modal('show');
+}
+
+function closeModal() {
+    $('#addUserModal').modal('hide');
+}
+
+function editUser(id) {
+    $('#lblTitleModal').text('Editar usuario')
+    $('#addUserModal').modal('show'); 
+    console.log('id >', id);
+}
+
+function deleteUser(id) {
+    console.log('id >', id);
+}
